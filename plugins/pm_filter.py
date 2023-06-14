30d4b7db9edbeb8e0486e061846a7e960d829bfd
Susil2510 <132734591+Susil2510@users.noreply.github.com>
2023-06-14 16:40:01 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -912,7 +912,7 @@ if imdb and imdb.get('poster'):
                 await asyncio.sleep(600)
                 await no_pic.delete()
                 await message.delete()
-    else:
+else:
         no_fil=await message.reply_photo(photo=NOR_IMG, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
         if settings["auto_delete"]:
             await asyncio.sleep(600)
